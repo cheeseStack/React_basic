85de4fbd3f8fc016983e96f5e26f7d2645e570d8
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-04-30 13:48:21 +0100

    added Navbar and Home js files

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = 'Welcome to the new blog';
@@ -8,17 +10,11 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1, 2, 3, 4, 5] }</p>
-        <p> { Math.random() * 10 }</p>
-
-        <a href={ link } target = "_blank" >Google site</a>
+        <Home />
+    
       </div>
     </div>
   );
