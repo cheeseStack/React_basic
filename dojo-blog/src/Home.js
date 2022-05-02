9767394ab0d8593fe99d3aa948970c5afb770879
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-02 22:22:09 +0100

    added isPending useState to true, then false when blogs loaded, to show a loading message whilst data was fetched

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect( () => {
         fetch('http://localhost:8000/blogs')
@@ -10,12 +11,14 @@ const Home = () => {
                 return response.json()
             })
             .then(data => {
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false);
             })
     }, []);
     
     return (  
         <div className="home">
+            {isPending && <div>Loading . . . </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" /> */}
             {/* <button onClick = { () => setName('luigi')} >Change name</button> */}
