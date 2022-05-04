1340c9cf1fe77fe839ca93bfd1d53faac2fded82
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-04 17:52:37 +0100

    added useState to the create blog form; use onChange function to target the inputted value for the name, body, author

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                {/* blog title */}
+                <label>Blog title:</label>
+                <input 
+                type="text" 
+                required
+                value= {title}
+                onChange = {(e) => setTitle(e.target.value) }
+                />
+                {/* blog body */}
+                <label>Blog body:</label>
+                <textarea 
+                required
+                value={ body }
+                onChange = { (e) => setBody(e.target.value)}>  
+                </textarea>
+                {/* blog author */}
+                <label> Blog author</label>
+                <select 
+                    value ={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>     
+                <button>Add Blog</button>
+                <p> { title }</p>
+                <p> { body }</p>
+                <p> { author }</p>
+
+            </form>
         </div>
      );
 }
