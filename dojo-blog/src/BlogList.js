4fb71597f6c783989553d457f03fd11a08aeb32e
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-04 16:36:04 +0100

    added BlogDetails.js file, imported useParams() function to output and link to a blog's id parameter

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs, title, handleDelete }) => { //shorthand method of the below 3 lines
 // const BlogList = (props) => {
@@ -9,8 +10,10 @@ const BlogList = ({ blogs, title, handleDelete }) => { //shorthand method of the
             <h2> { title }</h2>
             {blogs.map( (blog) => (
                 <div className="blog-preview" key={blog.id}>
+                    <Link to={ `/blogs/${blog.id}`}>
                     <h2>{ blog.title}</h2>
                     <p>Written by { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>
--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -31,7 +31,9 @@ const useFetch = (url) => {
                     setError(err.message);
                 }
             })
-    }, [url]);
+    },
+    
+    [url]);
 
     return { data, isPending, error };
 }
