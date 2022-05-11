1cdb374c9a575233ed10800e5ab255b8dd5eda3a
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-11 22:43:49 +0100

    added Link to Homepage from the title / logo

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <Link to="/"><h1>The Dojo Blog</h1></Link>
             <div className="links">
                 {/* <a href="/">Home</a> */}
                 <Link to="/">Home</Link>
