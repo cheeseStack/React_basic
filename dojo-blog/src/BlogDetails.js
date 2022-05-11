ebf447d14feb809cc74944944c8bb0e73b87c12b
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-11 22:15:43 +0100

    added a Delete button to the BlogDetails.js file, theat then navigates back to the Homepage

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from 'react-router-dom';
+
 
 const BlogDetails  = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:3001/blogs/' + id);
+
+    const navigate = useNavigate(); 
+    const handleDelete = ( ) => {
+        fetch('http://localhost:3001/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then( () => {
+            navigate({pathname: '/'}); // this navigate to the Home page once deleted
+        })
+
+    };
 
     return (  
         <div className="blog-details">
@@ -14,6 +26,7 @@ const BlogDetails  = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div> { blog.body }</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
